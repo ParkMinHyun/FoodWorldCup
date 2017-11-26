b0759da1a986b156599c7c280c5f1031e89b4d72
박민현 <algusl92@naver.com>
2017-11-27 02:34:54 +0900

    Butterknife 라이브러리 추가 및 Binding, Listener 적용 in ResultFoodInfoActivity

--- app/src/main/java/com/example/parkminhyun/foodworldcup/ResultFoodInfoActivity.java
+++ app/src/main/java/com/example/parkminhyun/foodworldcup/ResultFoodInfoActivity.java
@@ -1,15 +1,21 @@
 package com.example.parkminhyun.foodworldcup;
 
-import android.app.ProgressDialog;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.JsResult;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class ResultFoodInfoActivity extends AppCompatActivity {
+
+    @BindView(R.id.fab_review)
+    FloatingActionButton reviewBtn;
 
     private static final String TAG = "ResultFoodInfoActivity";
 
@@ -19,6 +25,7 @@ public class ResultFoodInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_food_info);
+        ButterKnife.bind(this);
 
         // 검색 URL 받기
         storeURL = getIntent().getExtras().getString("StoreURL");
@@ -31,4 +38,9 @@ public class ResultFoodInfoActivity extends AppCompatActivity {
         webView.loadUrl(storeURL);
     }
 
+
+    @OnClick(R.id.fab_review)
+    void reviewButtonClick() {
+        Toast.makeText(getApplicationContext(),"clicekced",Toast.LENGTH_SHORT).show();
+    }
 }
