387fe958dee0b6ce0230ed15d2cc4ebe26248914
안태현 <antaehyeon@live.co.kr>
2017-11-26 22:20:14 +0900

    Update MainActivity.java - 페이지 슬라이드 기능 추가 - res/anim 생성 및 파일 추가(translate_left, right)

--- app/src/main/java/com/example/parkminhyun/foodworldcup/MainActivity.java
+++ app/src/main/java/com/example/parkminhyun/foodworldcup/MainActivity.java
@@ -3,14 +3,70 @@ package com.example.parkminhyun.foodworldcup;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isPageOpen = false;
+
+    Animation translateLeftAnim;
+    Animation translateRightAnim;
+
+    LinearLayout mainLayout;
+
+    ImageButton myPageButton;
+
+    ImageView myPageimageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myPageButton = (ImageButton) findViewById(R.id.imgBtn_myPage);
+        myPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isPageOpen) {
+                    mainLayout.startAnimation(translateRightAnim);
+                } else {
+                    mainLayout.setVisibility(View.VISIBLE);
+                    mainLayout.startAnimation(translateLeftAnim);
+                }
+            }
+        });
 
+        mainLayout = (LinearLayout) findViewById(R.id.layout_myPage);
+
+        // 애니메이션 객체 로딩
+        translateLeftAnim = AnimationUtils.loadAnimation(this, R.anim.main_translate_left);
+        translateRightAnim = AnimationUtils.loadAnimation(this, R.anim.main_translate_right);
+
+        SlidingPageAnimationListener animListener = new SlidingPageAnimationListener();
+        translateLeftAnim.setAnimationListener(animListener);
+        translateRightAnim.setAnimationListener(animListener);
+
+    }
+
+    private class SlidingPageAnimationListener implements Animation.AnimationListener {
+        public void onAnimationEnd(Animation animation) {
+            if (isPageOpen) {
+                mainLayout.setVisibility(View.INVISIBLE);
+                isPageOpen = false;
+            } else {
+                isPageOpen = true;
+            }
+        }
+
+        public void onAnimationRepeat(Animation animation) {
+        }
+
+        public void onAnimationStart(Animation animation) {
+        }
     }
 }
